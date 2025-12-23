f0fb59715a5c8a25ef423bb3639f9659239b2dce
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 09:31:59 -0300

    feat: adição base das interfaces da repository

---
+++ ServicoPix.Domain/Interfaces/IRepositories/IContaRepository.cs
@@ -0,0 +1,9 @@
+﻿using ServicoPix.Domain.Entities;
+
+namespace ServicoPix.Domain.Interfaces.IRepositories;
+
+public interface IContaRepository
+{
+    Task<Conta?> ObterPorIdAsync(Guid id);
+    void Atualizar(Conta conta);
+}
---
+++ ServicoPix.Domain/Interfaces/IRepositories/ITransacaoRepository.cs
@@ -0,0 +1,9 @@
+﻿using ServicoPix.Domain.Entities;
+
+namespace ServicoPix.Domain.Interfaces.IRepositories;
+
+public interface ITransacaoRepository
+{
+    Task AdicionarAsync(Transacao transacao);
+    Task<Transacao?> ObterPorIdAsync(Guid id);
+}
