a60f85f2f98292aad38d61fd24511ece5edbd954
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 09:59:50 -0300

    feat: add eventos base

---
+++ ServicoPix.Domain/Events/PixRealizadoEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace ServicoPix.Domain.Events;
+
+public class PixRealizadoEvent
+{
+    public Guid TransacaoId { get; set; }
+    public Guid ContaOrigemId { get; set; }
+    public Guid ContaDestinoId { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataProcessamento { get; set; }
+    public string Status { get; set; }
+}
