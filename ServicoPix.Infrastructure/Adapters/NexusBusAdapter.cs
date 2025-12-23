613de894f31574e1ba49cb1ac19c0eb9c1552764
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 10:01:07 -0300

    feat: add adapters base

---
+++ ServicoPix.Infrastructure/Adapters/NexusBusAdapter.cs
@@ -0,0 +1,24 @@
+﻿using NexusBus.Abstractions;
+using ServicoPix.Domain.Interfaces.Services;
+
+namespace ServicoPix.Infrastructure.Adapters;
+
+public class NexusBusAdapter : IMensageriaService
+{
+    private readonly INexusBus _nexusClient;
+
+    public NexusBusAdapter(INexusBus nexusClient)
+    {
+        _nexusClient = nexusClient;
+    }
+
+    public async Task PublicarComandoAsync<T>(string fila, T mensagem)
+    {
+        await _nexusClient.PublishAsync(fila, mensagem);
+    }
+
+    public async Task PublicarEventoAsync<T>(string topico, T mensagem)
+    {
+        //await _nexusClient.Stream.PublishAsync(topico, mensagem);
+    }
+}
