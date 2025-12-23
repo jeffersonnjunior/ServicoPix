47585c1c420c61c606e9f617ce59b78e99794a6f
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 09:45:01 -0300

    feat: add base serviços mensageria

---
+++ ServicoPix.Domain/Interfaces/IServices/IMensageriaService.cs
@@ -0,0 +1,7 @@
+﻿namespace ServicoPix.Domain.Interfaces.Services;
+
+public interface IMensageriaService
+{
+    Task PublicarComandoAsync<T>(string fila, T mensagem);
+    Task PublicarEventoAsync<T>(string topico, T mensagem);
+}
