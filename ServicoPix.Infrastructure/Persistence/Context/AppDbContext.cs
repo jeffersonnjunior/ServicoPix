ea5c3e632be8fc44d92c1931706013938ce95a41
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 13:31:54 -0300

    feat: add context

---
+++ ServicoPix.Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServicoPix.Infrastructure.Persistence.Context;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options)
+    : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
