424937c9332406b0eb14085c57173c8b5686bce7
Jefferson Junior <jeffcwb1302jr@gmail.com>
2025-12-23 16:53:45 -0300

    feat: adição migration

---
+++ ServicoPix.Infrastructure/Persistence/Migrations/20251223195109_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServicoPix.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Numero = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Saldo = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContaOrigemId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContaDestinoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Valor = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    MensagemErro = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contas_Numero",
+                table: "Contas",
+                column: "Numero",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_ContaDestinoId",
+                table: "Transacoes",
+                column: "ContaDestinoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_ContaOrigemId",
+                table: "Transacoes",
+                column: "ContaOrigemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacoes_DataCriacao",
+                table: "Transacoes",
+                column: "DataCriacao");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contas");
+
+            migrationBuilder.DropTable(
+                name: "Transacoes");
+        }
+    }
+}
